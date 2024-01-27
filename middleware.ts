cdea8cbebb5b5f3b914f3c1cf198f089bd4b6b3e
Ladalo-Daniel <balaladalo@gmail.com>
2024-01-27 17:10:36 +0100

    login updates

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,26 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
+import { getProfile } from './supabase/user'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
+  const profile = await getProfile()
 
   const {
     data: { user },
   } = await supabase.auth.getUser()
 
-  // if user is signed in and the current path is / redirect the user to /account
-  if (user && req.nextUrl.pathname === '/') {
+  if (user && req.nextUrl.pathname === '/sign-up') {
+
+    if(profile?.data?.onboarded === true && user.id){
+      return NextResponse.redirect( new URL("/dashboard", req.url))
+    }
+
     return NextResponse.redirect(new URL('/account', req.url))
   }
 
-  // if user is not signed in and the current path is not / redirect the user to /
   if (!user && req.nextUrl.pathname !== '/') {
     return NextResponse.redirect(new URL('/', req.url))
   }
